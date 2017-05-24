769ad8820c5d18d1821e03b50f26387eb8a1f0e3
Tommy Ku <zoo1929@outlook.com>
2017-05-24 20:18:56 +0800

    FIX: react devtool compatibility

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import {render} from 'react-dom';
-import App from './app.jsx';
+import React from 'react'
+import {render} from 'react-dom'
+import App from './app.jsx'
+import 'preact/devtools'
+
 
 render(<App/>, document.getElementById('app'));
 
