aa16f936fe22f69a35e559f68ac8f432046bcf18
Tommy Ku <zoo1929@outlook.com>
2017-09-26 22:44:51 +0800

    ADD: hide list if no search result

--- src/pages/search_result_page.jsx
+++ src/pages/search_result_page.jsx
@@ -3,8 +3,8 @@ import TodoItem from '../todo_item.jsx';
 import { Link } from 'react-router-dom';
 
 class SearchResultPage extends PureComponent {
-  constructor(props) {
-    super(props);
+  searchResultStyle(resultCount) {
+    return { display: resultCount === 0 ? 'none' : 'inherit' };
   }
 
   render() {
@@ -14,15 +14,17 @@ class SearchResultPage extends PureComponent {
       <div>
         {
           Object.keys(results).map((key)=> (
-            <div key={`search-list-${key}`}>
+            <div key={`search-list-${key}`} style={ this.searchResultStyle(results[key].length) }>
               <p>
-                <Link to={`/list/${key}`}>{ lists[key].name }</Link>
+                列 <Link to={`/list/${key}`}>{ lists[key].name }</Link>
               </p>
               <p>{ results[key].length == 0 && '無呀' }</p>
               { results[key].map((todo)=> (<TodoItem listKey={key} {...todo} />)) }
             </div>
           ))
         }
+        { Object.values(results).reduce((memo, result)=> (memo + result.length), 0) == 0
+            && (<div>無呀</div>) }
       </div>
     );
   }
